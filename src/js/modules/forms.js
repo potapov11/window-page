77c764cba87fe05b52802c689b69743df3ea43c7
Serezha <volos11223344@yandex.ru>
2023-05-03 22:19:41 +0300

    promise

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -8,9 +8,41 @@ const forms = () => {
     failure: 'Что-то пошло не так',
   };
 
+  const postData = async (url, data) => {
+    document.querySelector('.status').textContent = message.loading;
+    let res = await fetch(url, {
+      method: 'POST',
+      body: data,
+    });
+
+    return await res.text();
+  };
+
+  const clearInputs = () => {
+
+  };
+
   form.forEach(item => {
     item.addEventListener('submit', (e) => {
+      e.preventDefault();
 
+      //Создаем элемент с сообщением
+      let statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      item.appendChild(statusMessage); //! Помещаем блок в конец формы с которой работаем
+
+      //Собираем данные из формы
+      const formData = new FormData(item); 
+
+      postData('assets/server.php', formData)
+          .then(res => {
+            console.log(res);
+            statusMessage.textContent = message.success;
+          })
+          .catch(() => statusMessage.textContent = message.failure)
+          .finally(() => {
+
+          });
     });
   });
 };
